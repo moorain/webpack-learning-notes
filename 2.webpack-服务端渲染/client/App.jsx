45d14ebef2ac5ca6748099a34d8fe48fa2b0023f
“Morain <y>
2018-09-02 10:56:26 +0800

    firstcommit

---
+++ 2.webpack-服务端渲染/client/App.jsx
@@ -0,0 +1,12 @@
+// 声明整个应用页面上的内容
+import React from 'react'
+
+class App extends React.Component{
+    render(){
+        return (
+           <div>this is app</div>
+        )
+    }
+}
+
+export default App;
